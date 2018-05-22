7244d32a4f07a17f7ad10b04e7120c763520f105
Philipp Kretzschmar <philipp.kretzschmar@dreamlines.de>
2018-05-22 19:15:40 +0200

    rm nconf

--- src/config/ConfigLoader.ts
+++
@@ -1,9 +0,0 @@
-import {Provider} from "nconf";
-
-const nconf = require("nconf");
-nconf
-    .argv()
-    .env()
-    .file({file: "config/appConfig.json"});
-
-export const config: Provider = nconf;
